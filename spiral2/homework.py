2144d325570b0e12f4888ec441a5d6977728bad0
yourname <youremail@address.com>
2021-05-03 21:18:09 -0400

    changes

--- spiral2/homework.py
+++ spiral2/homework.py
@@ -1,4 +1,18 @@
 def spiralize(size, n=1):
     """ Your code goes somewhere in here"""
+    size = []
+    x= sum(size)
+    counter = 0
+    incrt = 2
+    total = 0
+    
+    while x <= n **2:
+        total += x
+        x += incrt
+        counter += 1
+        if counter ==4:
+            incrt +=2
+            counter = 0
     return_value = n
     return return_value
+ 
